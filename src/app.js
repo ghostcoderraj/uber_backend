826cd038ba654912d250659af3d57c19e5c97a09
ghostcoderraj <officiallyaditya12@gmail.com>
2026-03-11 01:25:13 +0530

    Uber backend setup

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { connectDB } from "./config/db.config.js";
+import userRoutes from "./routes/user.routes.js";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+const app = express();
+connectDB();
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
+
+//Base Route
+app.get("/",(req,res)=>{
+    res.json({message:"Welcome to the Uber Backend API"});
+})
+
+//Going to implementation our application api routes
+app.use("/api/v1/auth",userRoutes);
+
+export default app;
---
+++ src/server.js
@@ -0,0 +1,12 @@
+import http from "http";
+import app from "./app.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const port = process.env.PORT || 8080;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
